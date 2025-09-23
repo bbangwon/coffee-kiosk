3c4f2f54c554ef78515c817df824a08c8fddb8d5
youngil <youngil@claion.co.kr>
2025-09-24 02:42:33 +0900

    프로젝트 구조 생성 및 첫 페이지 작성

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      hello world
-    </div>
+    <main className="flex flex-col gap-2">
+      <div className="w-16">
+        <img src="/image/logo.png" />
+      </div>
+      <div>
+        <img src="/image/coffee-and-cake.png" />
+      </div>
+      <div className="p-4">
+        <a href="/drink" className="bg-red-300 p-6 flex justify-center">
+          주문하기
+        </a>
+      </div>
+    </main>
   );
 }
